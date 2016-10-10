665a6c09c7988e0408da6dd163b0335ab1022c00
Dheeraj Kumar Gunti <guntidheerajkumar@gmail.com>
2016-10-10 21:35:30 +0530

    Removed unnecessary usings.

--- KBProgressLoader/ProgressLoader.cs
+++ KBProgressLoader/ProgressLoader.cs
@@ -20,8 +20,6 @@
 //OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //SOFTWARE.
 
-using System.Drawing;
-using CoreAnimation;
 using UIKit;
 using CoreGraphics;
 using System;
